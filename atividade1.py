a5e3d30a82e143e178a6ece69bd49ba699180411
Dama_kontzen <damarisdebittencourtkontzen@gmail.com>
2025-05-23 16:57:54 -0300

    atividade1.py

--- atividade1.py
+++ atividade1.py
@@ -1,9 +1,10 @@
+nome = str (input('Digite seu nome:'))
 
-nome = str (input('Digite seu nome'))
+serie = int (input('Digite sua serie:'))
 
-serie = int (input('Digite sua serie'))
-
-idade = int (input('Digite sua idade') )
+idade = int (input('Digite sua idade:') )
 
   
 print (nome, serie, idade)
+
+print ('seu nome é', nome, 'voce é da:', serie, ' e tem', idade, 'anos')
