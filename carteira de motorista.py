1afef060dbf1307aef8f76c7846ad5267d5e9419
Dama_kontzen <damarisdebittencourtkontzen@gmail.com>
2025-05-30 16:27:21 -0300

    Create carteira de motorista.py

---
+++ carteira de motorista.py
@@ -0,0 +1,8 @@
+nome = input ("Digite seu nome :")
+idade = float (input ("Digite sua idade :"))
+carteira = input ("você possue carteira de motorista :")
+if idade >18 and carteira == "sim":
+    print ( nome,"você já é de maior e está apto para dirigir")
+else: 
+    print ("ERRO!! você é menor de idade e não tem idade para dirigir ")
+
