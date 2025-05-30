2792c65f2e0b37393abb6c72c5830bb19924cb15
Dama_kontzen <damarisdebittencourtkontzen@gmail.com>
2025-05-30 16:09:39 -0300

    Create notas média do aluno.py

---
+++ notas média do aluno.py
@@ -0,0 +1,17 @@
+nome = input ("Digite seu nome :")
+nota1 = float (input("Digite sua primeira nota :"))
+nota2 = float (input("Digite sua segunda nota :"))
+media=(nota1 + nota2)/2
+print ("nome:", nome)
+print ("Sua primeira nota é:", nota1)
+print ("Sua segunda nota é:", nota2)
+print ("Sua média é :", media)
+if media >7:
+    print ("Aprovado")
+        
+else:
+     print ("Reprovado")
+
+        
+        
+    
