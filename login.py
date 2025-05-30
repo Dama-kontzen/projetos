2aaa3a555bf6fd3fc157682d5651af753fd620b7
Dama_kontzen <damarisdebittencourtkontzen@gmail.com>
2025-05-30 15:51:22 -0300

    Create login.py

---
+++ login.py
@@ -0,0 +1,11 @@
+login=(input("Digite seu login:"))
+senha=(input("Digite sua senha:"))
+if login=="dama" and senha == "1905":
+        print ("senha correta")
+        print ("Login correto")
+        
+else:
+        print("Erro, Revisar")
+        
+        
+    
