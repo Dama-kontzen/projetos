8987fe41845a2d4c5453210d84b138075533af14
Dama_kontzen <damarisdebittencourtkontzen@gmail.com>
2025-06-27 16:37:37 -0300

    Create livroDicionario.py

---
+++ livroDicionario.py
@@ -0,0 +1,10 @@
+livro = {
+    
+"titulo": "O Pequeno Príncipe",
+"autor": "Antoine de Saint-Exupéry",
+"ano_publicacao": 1943
+}
+
+livro ["genero"]="fabula"
+print (livro)
+print (f"autor{livro['autor']}")
