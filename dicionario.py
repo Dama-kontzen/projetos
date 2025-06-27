6bc77a4639970cdfe84536d71b1b643c6d443418
Dama_kontzen <damarisdebittencourtkontzen@gmail.com>
2025-06-27 15:49:03 -0300

    Create dicionario.py

---
+++ dicionario.py
@@ -0,0 +1,9 @@
+produto = {
+    "nome" : "Notbook Dell",
+    "preco" : 45000,
+    "estoque": 15,
+    "Cor":"prata"
+} 
+print (produto)
+print (f"nome: {produto["nome"]}")
+print (f"preco: {produto['preco']}")
