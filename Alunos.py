143cdac1665db6deebe18bf126e143cdb6abfc7b
Dama_kontzen <damarisdebittencourtkontzen@gmail.com>
2025-06-27 16:47:39 -0300

    Create Alunos.py

---
+++ Alunos.py
@@ -0,0 +1,10 @@
+Alunos = {
+    
+"nome": "Carlos Eduardo",
+"idade": 16,
+"turma" :"2A",
+}
+Alunos["disciplina_favorita":]="matematica"
+Alunos["idade"]=17
+print (Alunos)
+print (f"turma{Alunos['turma']}")
